fb7acaa608672eda95b5d28179fc7f70191a81bf
Julien Dehos <dehos@univ-littoral.fr>
2018-03-10 12:49:56 +0100

    pgphptest

--- nixops/pgphptest/nixops.nix
+++ nixops/pgphptest/nixops.nix
@@ -3,7 +3,7 @@
 
   webserver = { config, pkgs, ... }: 
   let
-    _mypage = import ./pgphptest.nix { inherit pkgs; };
+    _pgphptest = import ./pgphptest.nix { inherit pkgs; };
   in
   {
     networking.firewall.allowedTCPPorts = [ 80 ];
@@ -11,7 +11,8 @@
     services.httpd = {
       enable = true;
       adminAddr = "toto@example.com";
-      documentRoot = "${_mypage}/public";
+      #documentRoot = "/data/public";
+      documentRoot = "${_pgphptest}/public";
       extraModules = [
         { name = "php7"; path = "${pkgs.php}/modules/libphp7.so"; }
       ];
@@ -20,7 +21,7 @@
     services.postgresql = {
       enable = true;
       package = pkgs.postgresql;
-      initialScript = "${_mypage}/private/music.sql";
+      initialScript = "${_pgphptest}/private/music.sql";
       enableTCPIP = true;
       authentication = "local all all trust";
     };
--- nixops/pgphptest/pgphptest.nix
+++ nixops/pgphptest/pgphptest.nix
@@ -1,2 +1,12 @@
-# TODO
+{ pkgs ? import <nixpkgs> {} } :
+
+with pkgs;
+stdenv.mkDerivation {
+  name = "pgphptest";
+  src = ./.;
+  installPhase = ''
+    mkdir -p $out
+    cp -r data/* $out
+  '';
+}
 
