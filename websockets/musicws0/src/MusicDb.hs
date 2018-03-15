6369dc5f6d74fffa2649fd9dce9b11b27b29a80b
Julien Dehos <dehos@univ-littoral.fr>
2018-03-15 14:37:15 +0100

    menage websockets

--- websockets/musicws0/src/MusicDb.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module MusicDb (params) where
-
-import Data.ByteString as B
-
-params :: B.ByteString
-params = "host='localhost' port=5432 dbname=music_db user=music_user password='toto'" 
-
