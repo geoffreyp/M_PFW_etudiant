a2beda52d45cb700153d98a3ab5277e1ac6e0aa6
Geoffrey Pruvost <geoffrey@pruvost.xyz>
2018-03-16 15:14:00 +0100

    add musicws with update from upstream

--- websockets/musicws/src/musicws.hs
+++ websockets/musicws/src/musicws.hs
@@ -5,6 +5,7 @@ import qualified Data.Text as T
 import qualified Database.SQLite.Simple as SQL
 import qualified Network.WebSockets as WS
 
+
 main :: IO ()
 main = putStrLn "running server..." >> WS.runServer "0.0.0.0" 9000 application
 
@@ -14,9 +15,12 @@ application pc = WS.acceptRequest pc >>= forever . handleConnection
 handleConnection :: WS.Connection -> IO ()
 handleConnection connection = do
   msg <- WS.receiveDataMessage connection
-  let titles = [("artist1", "title1"), ("artist2", "title2")]
-  WS.sendTextData connection $ format titles
--- TODO select titles in database
+  conn <- SQL.open "music1.db"
+  let selectReq = "SELECT t.name, a.name FROM titles t, artists a WHERE t.name LIKE (?) AND a.id = t.artist"
+  r <- SQL.query conn selectReq (SQL.Only (T.concat ["%",decodeMsg msg,"%"]:: T.Text)) :: IO [DbTitle]
+  print r
+  WS.sendTextData connection $ format r
+
 
 type DbTitle = (T.Text, T.Text)
 
